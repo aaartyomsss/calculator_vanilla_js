60d3917ee74ee9b5c12adee4ad5df1ef318f191c
Artjom Savin <aaartyomsss@Artjoms-MacBook-Air.local>
2020-08-19 19:40:49 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let input1 = document.getElementById('input')
+let firstNum = 0;
+let secondNum = 0;
+let operation;
+
+let placeholder = document.querySelector('#placeholder')
+
+let pluss = document.querySelector('#pluss');
+let minus = document.querySelector('#minus');
+let multiply = document.querySelector('#mp-ly');
+let divide = document.querySelector('#divide');
+let result = document.querySelector('#result');
+
+// Setting operation to + and setting first number to variable
+pluss.addEventListener('click', plussSign);
+
+function plussSign() {
+    console.log(input1.value);
+    firstNum = input1.value;
+    input1.value = '';
+    operation = '+';
+}
+
+// Getting and displaying a result
+result.addEventListener('click', gettingResult);
+
+function gettingResult() {
+    secondNum = input1.value;
+    if (operation === '+') {
+        let final = parseInt(firstNum, 10) + parseInt(secondNum, 10);
+        final.toString();
+        placeholder.textContent = final;
+    }
+}
+
+
+
+
+
